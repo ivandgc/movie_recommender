a1c4d58d9c383d8d07e89fe0f68a09d1ddd25efb
sbal13 <sbal13@gmail.com>
2017-09-12 14:07:00 -0400

    Error handling

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
 	has_many :watched_movies
 	has_many :users, through: :watched_movies
+
+	
 	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
 
 	has_secure_password
 
+	validates :username, :email, uniqueness: true
 	
 end
